819b24f7c5603aba230086a0fa28ebb8065b7819
Rafael Carrilho <carrilhorafael57@gmail.com>
2023-07-24 22:41:38 -0700

    how to structure my resolvers

--- server.js
+++ server.js
@@ -8,36 +8,22 @@ const typesArray = loadFilesSync('**/*', {
   extensions: ['graphql'],
 });
 
+const resolversArray = loadFilesSync('**/*', {
+  extensions: ['resolvers.js'],
+});
+
 const schema = makeExecutableSchema({
   typeDefs: typesArray,
-  resolvers: {
-    Query: {
-      products: async (parent, args, context, info) => {
-        console.log('Getting the products...');
-        return await Promise.resolve(parent.products);
-      },
-      orders: async (parent, args, context, info) => {
-        console.log('Getting orders...');
-        return await Promise.resolve(parent.orders);
-      },
-    },
-  },
+  resolvers: resolversArray,
 });
 
 const port = 3000;
-
-const root = {
-  products: require('./products/products.model'),
-  orders: require('./orders/orders.model'),
-};
-
 const app = express();
 
 app.use(
   '/graphql',
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
   })
 );
